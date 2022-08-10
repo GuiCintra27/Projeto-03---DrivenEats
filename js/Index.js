4693b43af74ba733181fe8803880a3b717d87273
Guilherme Cintra <gui7lk@gmail.com>
2022-08-10 17:51:12 -0300

    codigo js melhorado, abrindo o pedido no wpp também (Atualizando pq ainda agr perdi o código e tive q fazer de novo)

--- js/Index.js
+++ js/Index.js
@@ -2,48 +2,28 @@ let CloseOrder = document.querySelectorAll('.Selected');
 let off_btn = document.getElementById('Off-button');
 let cl_o_btn = document.getElementById('Close-order-button');
 
-function Food(x){
-    
-    for (i = 1; i <= 5; i++){
-        y = document.getElementById('P'+i);
-        let Check = 'CP'+i;
-        Check = document.getElementById(Check);
+function Order(x){
+    let Options = ['P', 'B', 'S'];
 
-        if (y === x){
-            x.classList.toggle('Selected');
-            Check.classList.toggle('Off');
-        }
-        else{
-            y.classList.remove('Selected');
-            Check.classList.add('Off');
-        }
-    }
+    for (let option of Options){
+        for( l = 1; l <= 5; l++){
+            let z = document.getElementById(option+l);
 
-    CloseOrder = document.querySelectorAll('.Selected');
-
-    if (CloseOrder.length === 3){
-        off_btn.classList.add('Off');
-        cl_o_btn.classList.remove('Off');
-    } else{
-        off_btn.classList.remove('Off');
-        cl_o_btn.classList.add('Off');
-    }
-}
-
-function Drink(x){
-    
-    for (i = 1; i <= 5; i++){
-        y = document.getElementById('B'+i);
-        let Check = 'CB'+i;
-        Check = document.getElementById(Check);
-
-        if (y === x){
-            x.classList.toggle('Selected');
-            Check.classList.toggle('Off');
-        }
-        else{
-            y.classList.remove('Selected');
-            Check.classList.add('Off');
+            if (z === x){
+                for (i = 1; i <= 5; i++){
+                    let y = document.getElementById(option+i);
+                    let Check = 'C'+option+i;
+                    Check = document.getElementById(Check);
+            
+                    if (y === x){
+                        x.classList.toggle('Selected');
+                        Check.classList.toggle('Off');
+                    } else{
+                        y.classList.remove('Selected');
+                        Check.classList.add('Off');
+                    }
+                }
+            }
         }
     }
 
@@ -58,30 +38,11 @@ function Drink(x){
     }
 }
 
-function Dessert(x){
-    
-    for (i = 1; i <= 5; i++){
-        y = document.getElementById('S'+i);
-        let Check = 'CS'+i;
-        Check = document.getElementById(Check);
+let text = 'Olá, gostaria de fazer o pedido: \n - Prato: \n- Bebida: \n - Sobremesa: \n Total: ';
+let link = "https://wa.me/5522992710166?text=";
+let url = encodeURIComponent(text);
 
-        if (y === x){
-            x.classList.toggle('Selected');
-            Check.classList.toggle('Off');
-        }
-        else{
-            y.classList.remove('Selected');
-            Check.classList.add('Off');
-        }
-    }
-
-    CloseOrder = document.querySelectorAll('.Selected');
-    
-    if (CloseOrder.length === 3){
-        off_btn.classList.add('Off');
-        cl_o_btn.classList.remove('Off');
-    } else{
-        off_btn.classList.remove('Off');
-        cl_o_btn.classList.add('Off');
-    }
+function Finish(){
+    const whatsapp = window.open(link + url, '_blank');
+    whatsapp.focus();
 }
