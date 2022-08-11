1260b0c2e4dbae58d84ebb364fb409074be4e531
Guilherme Cintra <gui7lk@gmail.com>
2022-08-10 22:43:14 -0300

    Projeto Finalizado

--- js/Index.js
+++ js/Index.js
@@ -2,7 +2,15 @@ let CloseOrder = document.querySelectorAll('.Selected');
 let off_btn = document.getElementById('Off-button');
 let cl_o_btn = document.getElementById('Close-order-button');
 
-function Order(x){
+let food = 0;
+let drink = 0;
+let dessert = 0;
+
+let prato = '';
+let bebida = '';
+let sobremesa = '';
+
+function Order(x,b){
     let Options = ['P', 'B', 'S'];
 
     for (let option of Options){
@@ -18,6 +26,22 @@ function Order(x){
                     if (y === x){
                         x.classList.toggle('Selected');
                         Check.classList.toggle('Off');
+                        let a = document.getElementById('P'+option+i);
+                        a = String(a.innerHTML);
+                        a = a.substr(3);
+                        a = parseFloat(a);
+                        
+                        if (option === 'P'){
+                            food = a;
+                            prato = b.innerHTML;
+                        } else if(option === 'B'){
+                            drink = a;
+                            bebida = b.innerHTML;
+                        } else{
+                            dessert = a;
+                            sobremesa = b.innerHTML;
+                        }
+
                     } else{
                         y.classList.remove('Selected');
                         Check.classList.add('Off');
@@ -38,11 +62,11 @@ function Order(x){
     }
 }
 
-let text = 'Olá, gostaria de fazer o pedido: \n - Prato: \n- Bebida: \n - Sobremesa: \n Total: ';
-let link = "https://wa.me/5522992710166?text=";
-let url = encodeURIComponent(text);
-
 function Finish(){
+    let text = 'Olá, gostaria de fazer o pedido: \n- Prato: ' + prato + '\n- Bebida: ' + bebida + '\n - Sobremesa: ' + sobremesa +'\n Total: ' + (food + drink + dessert).toFixed(2);
+    let link = "https://wa.me/5522992710166?text=";
+    let url = encodeURIComponent(text);
+    
     const whatsapp = window.open(link + url, '_blank');
     whatsapp.focus();
 }
